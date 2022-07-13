4d365b84f38df2417375c72c3fa20b413b8b9568
mRezaAnanta <muhammadRezaAnanta@tutamail.com>
2022-07-14 01:11:20 +0700

    updated code

--- services/index.js
+++ services/index.js
@@ -158,4 +158,66 @@ export const getComments = async (slug) => {
     const result = await request(graphqlAPI, query, { slug })
 
     return result.comments;
-}
+};
+
+export const getFeaturedPosts = async () => {
+    const query = gql`
+      query GetCategoryPost() {
+        posts(where: {featuredPost: true}) {
+          author {
+            author_name
+            author_photo {
+              url
+            }
+          }
+          featuredImage {
+            url
+          }
+          post_title
+          slug
+          createdAt
+        }
+      }   
+    `;
+  
+    const result = await request(graphqlAPI, query);
+  
+    return result.posts;
+  };
+
+  export const getCategoryPost = async (slug) => {
+    const query = gql`
+      query GetCategoryPost($slug: String!) {
+        postsConnection(where: {categories_some: {slug: $slug}}) {
+          edges {
+            cursor
+            node {
+              author {
+                bio
+                author_name
+                id
+                author_photo {
+                  url
+                }
+              }
+              createdAt
+              slug
+              post_title
+              post_excerpt
+              featuredImage {
+                url
+              }
+              categories {
+                name
+                slug
+              }
+            }
+          }
+        }
+      }
+    `;
+  
+    const result = await request(graphqlAPI, query, { slug });
+  
+    return result.postsConnection.edges;
+};  
